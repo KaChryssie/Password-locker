fd3ab7a55f5a43d407708422634e7adcfda8a928
KaChryssie <karerassie@gmail.com>
2020-11-13 20:39:27 +0200

    create find method

--- credential.py
+++ credential.py
@@ -4,24 +4,20 @@ class Credential:
     """
     credential_list = [] #empty credential list
 
-    
+    def save_credential(self):
+
+        '''
+        save_credential method saves credential objects into credential list.
+        '''
+
+      
+
+        Credential.credential_list.append(self)    
+
+
     
     @classmethod
-    def find_by_name(cls,name):
-
-        '''
-       Method that takes in a name returns a credential that matches that name.
-       
-       Args:
-            name: name to search for
-        Returns:
-            Credential of the person that matches the name.
-            
-        '''
-        for credential in cls.credential_list:
-            if credential.account_name == name:
-                return credential  
-
+    
 
     @classmethod
     def credential_exist(cls,name):
